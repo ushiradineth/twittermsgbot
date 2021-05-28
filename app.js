790184bb726b56494455a18e5e52a747e79cbb43
ushiradineth <ushiradineth@gmail.com>
2021-05-28 23:11:26 +0530

    commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const fs = require("fs");
 const { google } = require("googleapis");
 const sheets = google.sheets("v4");
 const { TwitterClient } = require("twitter-api-client");
+const Filter = require("bad-words");
 const credentials = require("./credentials.json");
 const data = require("./data.json");
 
@@ -20,9 +21,23 @@ const authClient = new google.auth.JWT(
     credentials.private_key,
     ["https://www.googleapis.com/auth/spreadsheets"]
 );
-google.options({ auth: authClient }); // !!
+google.options({ auth: authClient });
 const token = authClient.authorize();
 authClient.setCredentials(token);
+
+// Profanity check declaration
+filter = new Filter();
+filter.addWords('suspend', 'deactivate', 'suspended', 'deactivated');
+
+// Recursive function for tweet handling !! might not be working
+async function tweet() {
+    try {
+        const tweet = await twitterClient.tweets.statusesUpdate({ status: status });
+    } catch {
+        await new Promise(r => setTimeout(r, 30 * 60 * 1000));
+        tweet();
+    }
+}
 
 setInterval(async function () {
     // All of the data from the form
@@ -33,7 +48,7 @@ setInterval(async function () {
         var res = await sheets.spreadsheets.values.get({
             auth: authClient,
             spreadsheetId: credentials.spreadsheetId,
-            range: "B" + data.rows+1 + ":E",
+            range: "B" + (++data.rows) + ":E",              // !! might break
         });
 
         // Set rows to equal the rows
@@ -41,53 +56,54 @@ setInterval(async function () {
 
         // Check if we have any data and if we do add it to our formData array
         if (typeof rows !== "undefined") {
-
-            // Remove the headers
-            rows.shift();
-
             // For each row
-            for (i = data.rows; i < rows.length; i++) {
-                    formData.push({ msg: rows[i][0], receiverHandle: rows[i][1], anonChoice: rows[i][2], senderHandle: rows[i][3] });
-                    
-                    // Delete the used data
-                    res = sheets.spreadsheets.values.batchClear({
-                        spreadsheetId: credentials.spreadsheetId,
-                        requestBody: {
-                            ranges: "A" + i + ":E" + i, // !! might fail here
-                        },
-                    });
-                }
-
+            for (i = 0; i < rows.length; i++) {
+                formData.push({
+                    msg: rows[i][0],
+                    receiverHandle: rows[i][1],
+                    anonChoice: rows[i][2],
+                    senderHandle: rows[i][3],
+                });
+            }
         } else {
             console.log("No data found.");
         }
 
-        for (i = data.row; i < formData.length && i >= data.rows; i++) {
+        for (i = 0; i < formData.length && i >= data.rows; i++) {
             if (formData[i].anonChoice == "No") {
                 formData[i].senderHandle = "[anon]";
             }
-    
+
             if (typeof formData[i].receiverHandle !== "undefined" || typeof formData[i].msg !== "undefined") {
-                const status = "from: " + formData[i].senderHandle + "\n to: " + formData[i].receiverHandle + "\n\n" + formData[i].msg;
-                console.log(i + " >= " + data.rows);
-                //   const tweet = await twitterClient.tweets.statusesUpdate({
-                //     status: status
-                //   });
+                const status =
+                    "from: " + formData[i].senderHandle +
+                    "\n to: " + formData[i].receiverHandle +
+                    "\n\n" + filter.clean(formData[i].msg);  
+
+                tweet(); // !! might not be working
             }
         }
-
-        // Storing the rows to a json file so the program can be restared at anytime
+        
         if (typeof rows !== "undefined") {
-            fs.writeFileSync('data.json', JSON.stringify({rows: rows.length}),
+            // Delete the used data
+            res = sheets.spreadsheets.values.batchClear({
+                spreadsheetId: credentials.spreadsheetId,
+                requestBody: {
+                    ranges: "A" + (++data.rows) + ":E" + rows.length,      // !! might break
+                },
+            });
+
+            // Storing the rows to a json file so the program can be restared at anytime
+            fs.writeFileSync(
+                "data.json",
+                JSON.stringify({ rows: rows.length }),
                 function (err, file) {
                     if (err) throw err;
                 }
             );
         }
-
     } catch (error) {
         // Log the error
         console.log(error);
     }
-    console.log("run");
-}, 10000); // 10 * 60 * 1000 Function repeats every ten minutes
+}, 20000); // 10 * 60 * 1000 Function repeats every ten minutes !! change
